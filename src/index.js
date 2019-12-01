3fa5ef8bd887917683ee52c217dd3c2da6c0397b
Otto Pecz <otto.pecz@gmail.com>
2019-12-01 20:19:10 +0000

    Adding Dockerfile

--- src/index.js
+++ src/index.js
@@ -1,5 +1,11 @@
+import http from 'http';
 import "core-js/stable";
 import "regenerator-runtime/runtime";
-import depA from './depA';
 
-console.log('index', depA);
+const server = http.createServer((req, res) => {
+  const ip = res.socket.remoteAddress;
+  const port = res.socket.remotePort;
+  res.end(`Your IP address is ${ip} and your source port is ${port}.`);
+})
+
+server.listen(3000);
